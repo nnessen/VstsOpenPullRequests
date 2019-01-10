f9893aa839c6cbac24ea09e5a67445d2d03d2182
Nick Nessen <nicholas.nessen@sprbrk.com>
2019-01-10 13:43:05 -0800

    Corrects comment counting and bumps version

--- src/services/table.service.ts
+++ src/services/table.service.ts
@@ -312,9 +312,7 @@ export class TableService {
         for (let j = 0; threads.length > j; j++) {
 
             let thread = threads[j];
-            if (!thread.properties) { continue; }
-            if (!thread.properties["Microsoft.TeamFoundation.Discussion.UniqueID"]) { continue; }
-            if (thread.isDeleted) { continue; }
+            if (!thread || thread.isDeleted) { continue; }
 
             let status = +thread.status;
             if (status >= 0) {
